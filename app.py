7c13c5e43ded62d454eb1ff2d3438348e4654625
olgavetoskina48-cmd <olgavetoskina48@gmail.com>
2026-06-28 05:11:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ SUPABASE_URL = "https://jzscsndwuchzlellgqea.supabase.co"
 SUPABASE_KEY = "sb_publishable_-kqOsr7gFZRi8ctCNPaLgg_4mjU-NZy"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-TOKEN = "8869752953:AAF2gOnS-bFts-EGsS1PZZ4pfUrRXLwkN-M"
 game_data = {}
 
 def get_pet(user_id):
